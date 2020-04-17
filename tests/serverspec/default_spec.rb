da2781812676d7fc3d2e47f06477e3f729520550
Tomoyuki Sakurai <y@trombik.org>
2020-04-17 22:30:34 +0900

    initial implementation

--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -2,57 +2,59 @@ require "spec_helper"
 require "serverspec"
 
 package = "portshaker"
-service = "portshaker"
-config  = "/etc/portshaker/portshaker.conf"
+config  = "/usr/local/etc/portshaker.conf"
 user    = "portshaker"
 group   = "portshaker"
-ports   = [PORTS]
-log_dir = "/var/log/portshaker"
-db_dir  = "/var/lib/portshaker"
+mirror_base_dir = "/var/cache/portshaker"
+ports_dirs = %w[
+  /usr/local/poudriere/ports/default
+]
 
-case os[:family]
-when "freebsd"
-  config = "/usr/local/etc/portshaker.conf"
-  db_dir = "/var/db/portshaker"
+describe group(group) do
+  it { should exist }
+end
+
+describe user(user) do
+  it { should exist }
+  it { should belong_to_group group }
 end
 
 describe package(package) do
   it { should be_installed }
 end
 
+describe file mirror_base_dir do
+  it { should exist }
+  it { should be_directory }
+  it { should be_mode 755 }
+  it { should be_owned_by user }
+  it { should be_grouped_into group }
+end
+
 describe file(config) do
   it { should be_file }
+  its(:content) { should match(/Managed by ansible/) }
   its(:content) { should match Regexp.escape("portshaker") }
 end
 
-describe file(log_dir) do
-  it { should exist }
-  it { should be_mode 755 }
-  it { should be_owned_by user }
-  it { should be_grouped_into group }
+describe command "portshaker -s" do
+  its(:exit_status) { should eq 0 }
+  its(:stderr) { should eq "" }
+  its(:stdout) { should match(/^portsnap$/) }
 end
 
-describe file(db_dir) do
-  it { should exist }
-  it { should be_mode 755 }
-  it { should be_owned_by user }
-  it { should be_grouped_into group }
+describe command "portshaker -t" do
+  its(:exit_status) { should eq 0 }
+  its(:stderr) { should eq "" }
+  its(:stdout) { should match(/^default$/) }
 end
 
-case os[:family]
-when "freebsd"
-  describe file("/etc/rc.conf.d/portshaker") do
-    it { should be_file }
-  end
-end
-
-describe service(service) do
-  it { should be_running }
-  it { should be_enabled }
-end
-
-ports.each do |p|
-  describe port(p) do
-    it { should be_listening }
+ports_dirs.each do |d|
+  describe file d do
+    it { should exist }
+    it { should be_directory }
+    it { should be_owned_by user }
+    it { should be_grouped_into group }
+    it { should be_mode 755 }
   end
 end
